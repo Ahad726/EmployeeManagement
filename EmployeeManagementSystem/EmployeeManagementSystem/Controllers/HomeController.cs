e4b2eb9002fd0eaa3dfba920634834c7fb82036c
Ahad726 <atmahad726@gmail.com>
2020-09-21 19:30:49 +0600

    Primary project setup

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using EmployeeManagementSystem.Models;
+using EmployeeManagementSystem.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace EmployeeManagementSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public ViewResult Index()
+        {
+            var model = _employeeRepository.GetAllEmployee();
+            return View(model);
+        }
+
+        public ViewResult Details(int? id)
+        {
+            HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
+            {
+                Employee = _employeeRepository.GetEmployee(id??1),
+                PageTitle = "Employee Details"
+
+            };
+
+            return View(homeDetailsViewModel);
+        }
+
+        public ViewResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ EmployeeManagementSystem/EmployeeManagementSystem/Models/MockEmployeeRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employee;
+
+        public MockEmployeeRepository()
+        {
+            _employee = new List<Employee>()
+            {
+                new Employee(){Id = 1, Name = "Atm", Email= "Atm@gmail.com", Department =Dept.HR},
+                new Employee(){Id = 2, Name = "Abdul Ahad", Email= "Abdul@gmail.com", Department =Dept.SE },
+                new Employee(){Id = 3, Name = "Talha", Email= "talha@gmail.com", Department = Dept.IT}
+            };
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _employee;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return _employee.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
